78a4ac4144e62db2ba7a2fb265f0fd485dce90ae
NyssFeed <NyssFeed@users.noreply.github.com>
2020-08-01 03:54:57 +0530

    Comming Soon Page Update

--- categories/others/search.js
+++ categories/others/search.js
@@ -2,7 +2,7 @@ function search() {
     let input = document.getElementById('searchbar').value 
         input=input.toLowerCase(); 
     let x = document.getElementsByTagName("h6"); 
-    let y = document.getElementsByClassName('a'); 
+    let y = document.getElementsByClassName('s'); 
       
     for (i = 0; i < x.length; i++) {  
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
